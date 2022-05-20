a388a604341e099d5fe3fc95d237a7941c557db8
Zahida Meeran <dev.zmeeran@gmail.com>
2022-05-19 20:14:29 -0400

    Adding stock api code

---
+++ stockapi.py
@@ -0,0 +1,16 @@
+import requests
+
+API_KEY = 'demo'
+r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=' + API_KEY)
+if (r.status_code == 200):
+    print(r.json())
+
+result = r.json()
+
+dataForAllDays = result['Time Series (Daily)']
+dataForSingleDate = dataForAllDays['2021-12-29']
+print(dataForSingleDate['1. open'])
+print(dataForSingleDate['2. high'])
+print(dataForSingleDate['3. low'])
+print(dataForSingleDate['4. close'])
+print(dataForSingleDate['5. volume'])
