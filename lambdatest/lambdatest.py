668c0218d534570e7298941755abda1bfc214f2a
Zahida Meeran <dev.zmeeran@gmail.com>
2022-05-22 17:23:48 -0400

    adding 1st lambda function api code and deployment zip file

---
+++ lambdatest/lambdatest.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def getstock(event, context):
+    print("uploaded code from local machine")
+
+    API_KEY = 'demo'
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
+
+
+    r = requests.get(url + API_KEY)
+    if (r.status_code == 200):
+        #print(r.json())
+        print("success")
+
+    result = r.json()
+
+
+    dataForAllDays = result['Time Series (Daily)']
+    dataForSingleDate = dataForAllDays['2021-12-29']
+    print(dataForSingleDate['1. open'])
+    print(dataForSingleDate['2. high'])
+    print(dataForSingleDate['3. low'])
+    
+    print(url)
